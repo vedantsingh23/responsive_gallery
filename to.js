17423e51cc2dad2b35b251feaed939466c7b2915
vedantsingh23 <141285386+vedantsingh23@users.noreply.github.com>
2024-02-28 17:52:17 -0700

    t

--- to.js
+++ to.js
@@ -1,4 +1,40 @@
-let slideIndex = 1;
-function vie (){
-    document.getElementById()
+let slide = 1;
+function openn(){
+    document.getElementById('images1').style.display = 'block';
 }
+
+function closee(){
+    document.getElementById('images1').style.display = 'none';
+}
+
+function nextSlide(n){
+    slideshow(slide += n)
+}
+
+function current(n){
+    slideshow(slide = n)
+
+}
+
+function slideshow(n){
+    let slides = document.getElementsByClassName('zondaa');
+    if(n>slides.length){
+        slide = 1
+    }
+
+    if(n<1){
+        slide = slides.length;
+    }
+
+    for(let i=0; i<slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+
+    slides[slide-1].style.display = 'block';
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    slideshow(slide)
+})
